b536d38e2126d633a0a16a6985f4337771bb55ca
Jack OByrne <127803374+JackOB95-lab@users.noreply.github.com>
2024-03-10 21:38:04 +0000

    java

---
+++ Java/javatest.js
@@ -0,0 +1,10 @@
+
+const name = prompt("What is your name?");
+
+function greet() {
+  const name = prompt("What is your name?");
+  const greeting = document.querySelector("#greeting");
+  greeting.textContent = `Hello ${name}, nice to see you!`;
+}
+
+button.addEventListener("click", greet);
